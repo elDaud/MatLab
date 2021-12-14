cf90489d14c6d60605fa70f7c5a6f06bc11ef425
elDaud <96099379+elDaud@users.noreply.github.com>
2021-12-14 09:58:09 +0530

    Create Abaqus2Matlab.m

---
+++ Abaqus2Matlab.m
@@ -0,0 +1,24 @@
+s = fileread('Abaqus.inp');
+q = split(s,'*');
+h = strfind(s,'*');
+k = strfind(s,'*Node');
+m = strfind(s,'*Element');
+elementStr = q{find(h==m(1))+1};
+elementStr = split(elementStr, newline);
+elem = zeros(length(elementStr)-2,9);
+for i = 2: length(elementStr)-1
+    a = str2double(string(split(elementStr(i,:))));
+    a(isnan(a)) = [];
+    elem(i-1,:) = a';
+end
+    
+nodeStr = q{find(h==k(1))+1};
+nodeStr = split(nodeStr, newline);
+nodes = zeros(length(nodeStr)-2,4);
+for i = 2: length(nodeStr)-1
+    a = str2double(string(split(nodeStr(i,:))));
+    a(isnan(a)) = [];
+    nodes(i-1,:) = a';
+end
+
+clearvars -except elem nodes
